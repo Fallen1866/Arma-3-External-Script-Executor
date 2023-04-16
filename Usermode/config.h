45d4fcdb8cf5fc165e2c69de46b2f404402842ac
susgsus <henpet2015@gmail.com>
2023-04-16 06:53:42 +0200

    Cleanup but no arrays yet

--- Usermode/config.h
+++ Usermode/config.h
@@ -7,14 +7,14 @@
 class Param {
 protected:
 
-	UINT64 m_Base = 0;
-	UINT64 vTableType = 0;
-	std::string name = "";
+	UINT64		m_Base			= 0;
+	UINT64		m_VTableType	= 0;
+	std::string m_Name			= "";
 
 	enum Type {
 		CLASS,
 		VALUE
-	}type;
+	}m_Type;
 
 public:
 
@@ -24,181 +24,67 @@ public:
 		m_Base = Base;
 	}
 
-	UINT64 GetBase() {
-		return m_Base;
-	}
+	UINT64			GetBase();
+	std::string*	GetName();
+	Type			GetType();
 
-	UINT64 GetVTableType() {
-		return vTableType;
-	}
+	UINT64			GetVTableType();
+	void			ReadName();
+	void			ReadType();
 
-	void ReadName() {
-		name = GArmaString(m_Base + 0x8).Get();
-	}
-
-	std::string* GetName() {
-		return &name;
-	}
-
-	void ReadType() {
-		vTableType = coms->Read<UINT64>(m_Base) - SDK->GetModuleBase();
-		if (vTableType == 0x1DC28D0) {
-			type = CLASS;
-		}
-		else
-		{
-			type = VALUE;
-		}
-	}
-
-	Type GetType() {
-		return type;
-	}
-
-	virtual void Render() {};
+	virtual void	Render() {};
 };
 
 class ParamValue : public Param {
 private:
 
-	std::string value = "";
+	std::string m_Value = "";
 
 public:
 
 	ParamValue(UINT64 Base) {
-		m_Base = Base;
+		m_Base			= Base;
 	}
 
 	ParamValue(Param Param) {
-		m_Base = Param.GetBase();
-		name = *Param.GetName();
-		type = Param.GetType();
-		vTableType = Param.GetVTableType();
+		m_Base			= Param.GetBase();
+		m_Name			=*Param.GetName();
+		m_Type			= Param.GetType();
+		m_VTableType	= Param.GetVTableType();
 	}
 
-	void ReadValue() {
-		switch (vTableType)
-		{
-		case 0x1DC4A78:
-			value = std::to_string(coms->Read<UINT32>(m_Base + 0x10));
-			break;
-		case 0x1DC4528:
-			value += "\"" + GArmaString(m_Base + 0x10).Get() + "\"";
-			break;
-		case 0x1DC5108:
-			value = "ARRAY";	// todo read array (its an autoArray)
-			break;
-		case 0x1DC47D0:
-			value = std::to_string(coms->Read<float>(m_Base + 0x10));
-			break;
-		default:
-			value = "INVALID";
-			break;
-		}		
-	}
-
-	void Render(int depth = 0) {
-		if (!this->name.size()) {
-			return;
-		}
-
-		std::string indentation = "";
-		for (int i = 0; i < depth; ++i) {
-			indentation += "\t";
-		}
-
-		ImGui::Text("%s%s = %s;", indentation, this->name.c_str(), this->value.c_str());
-	}
+	void ReadValue();
+	void Render(int depth = 0);
 };
 
 class ParamClass : public Param {
 private:
 
-	std::vector<ParamClass>paramClasses;
-	std::vector<ParamValue>paramValues;
+	std::vector<ParamClass>m_ParamClasses;
+	std::vector<ParamValue>m_ParamValues;
 
 public:
 	ParamClass() {}
 
 	ParamClass(UINT64 Base) {
-		m_Base = Base;
+		m_Base	= Base;
 	}
 
 	ParamClass(Param Param) {
-		m_Base = Param.GetBase();
-		name = *Param.GetName();
-		type = Param.GetType();
+		m_Base	= Param.GetBase();
+		m_Name	=*Param.GetName();
+		m_Type	= Param.GetType();
 	}
 
-	void ReadContents() {
-		GAutoArray contentArray(m_Base + 0x48);
-		if (contentArray.InvalidList(10000)) {
-			return;
-		}
-
-		for (int i = 0; i < contentArray.GetSize(); ++i) {
-			Param value(contentArray.Get<UINT64>(i));
-
-			value.ReadName();
-			if (!value.GetName()->size()) {
-				continue;
-			}
-
-			value.ReadType();
-			if (value.GetType() == CLASS) {
-				ParamClass paramClass(value);
-				paramClass.ReadContents();
-
-				paramClasses.push_back(paramClass);
-			}
-			else {
-				ParamValue paramValue(value);
-				paramValue.ReadValue();
-
-				paramValues.push_back(paramValue);
-			}
-		}
-	}
-
-	void Render(int depth = 0) {
-		for (ParamValue& paramValue : paramValues) {
-			paramValue.Render(depth);
-		}
-
-		for (ParamClass& paramClass : paramClasses) {
-			std::string contentName = *paramClass.GetName();
-			if (!contentName.size()) {
-				continue;
-			}
-
-			std::string indentation = "";
-			for (int i = 0; i < depth; ++i) {
-				indentation += "\t";
-			}
-
-			std::string name = indentation + "class " + contentName;
-			ImGui::Text(name.c_str());
-
-			if (paramClass.paramClasses.size() || paramClass.paramValues.size()) {
-				std::string openingBrace = indentation + "{";
-				ImGui::Text(openingBrace.c_str());
-			}
-
-			paramClass.Render(depth + 1);
-
-			if (paramClass.paramClasses.size() || paramClass.paramValues.size()) {
-				std::string closingBrace = indentation + "};";
-				ImGui::Text(closingBrace.c_str());
-			}
-		}
-	}
+	void ReadContents();
+	void Render(int depth = 0);
 };
 
 class ConfigComponent : public MenuTab {
 private:
 
-	UINT64 m_Base = 0;
-	ParamClass MissionSQM;
+	UINT64		m_Base = 0;
+	ParamClass	m_MissionSQM;
 
 	UINT64		GetNewDescriptionExt();
 	ParamClass	GetMissionSQM();
